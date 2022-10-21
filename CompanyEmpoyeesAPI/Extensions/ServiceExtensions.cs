4248d3a2c0a41f68e67ba1218370688b8fa622fe
Дмитрий Корец <dimon.korecz@gmail.com>
2022-10-21 19:20:28 +0300

    Configure custom csv formatter

--- CompanyEmpoyeesAPI/Extensions/ServiceExtensions.cs
+++ CompanyEmpoyeesAPI/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using CompanyEmpoyeesAPI.Formatters;
+using Contracts;
 using Contracts.Repository;
 using Entities;
 using LoggerService;
@@ -34,5 +35,8 @@ namespace CompanyEmpoyeesAPI.Extensions
         }
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new
+            CsvOutputFormatter()));
     }
 }
--- CompanyEmpoyeesAPI/Startup.cs
+++ CompanyEmpoyeesAPI/Startup.cs
@@ -37,7 +37,12 @@ namespace CompanyEmpoyeesAPI
             services.ConfigureRepositoryManager();
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddControllers();
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters()
+              .AddCustomCSVFormatter();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
